5af42288e82a04d7daa9b7f7b471d1ea4226dd67
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-05-03 15:45:05 +0600

    form setup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { FaGoogle, FaGithub } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+
+
 
 const Login = () => {
+
+    // const {signIn} = useContext();
+    const navigate = useNavigate();
+    const location = useLocation();
+    console.log(location);
+    
+    const from = location.state?.from?.pathname || '/';
+    console.log(from);
+    
+    const handleLogin = event =>{
+        event.preventDefault();
+    
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    
+        signIn(email, password)
+        .then(result =>{
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            form.reset();
+            navigate(from, {replace: true});
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
+
     return (
         <div>
             <h3 className='text-center mt-4'>Welcome To Endless Chef Table!!</h3>
-            <Form className='w-50 mx-auto'>
+            <Form className='w-50 mx-auto' onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Register = () => {
     return (
         <div>
             <h2>Please Register</h2>
+            <Form className='w-50 mx-auto'>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label> User Name</Form.Label>
+                    <Form.Control type="name" placeholder="Enter Your Name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control type="password" placeholder="Photo URL" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Terms & Conditions" />
+                </Form.Group>
+                <Button variant="info" type="submit">
+                    Register
+                </Button>
+            </Form>
         </div>
     );
 };
