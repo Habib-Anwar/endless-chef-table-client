b62ef19add0706be48d1f2265f1f55bdf0004b0f
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-05-04 23:40:14 +0600

    404

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+    return (
+        <div>
+                <h1>Page not found!!
+            </h1>
+            <p>Go to the <Link to ="/">HomePage</Link></p>
+        </div>
+    );
+};
+
+export default NotFound;
